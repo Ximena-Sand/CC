44b7a7976913d20d44feaa95217d9237f909c7f6
Ximena-Sand <sandovalximena92@gmail.com>
2026-06-29 15:01:21 +0100

    first code

--- session_01/session_01_demo/sketch.js
+++ session_01/session_01_demo/sketch.js
@@ -1,8 +1,40 @@
 function setup() {
     createCanvas(400, 400);
+
+    let answer = 42; // Declared a variable 
+    let fav = 8;    // Number data type
+    
+    console.log(answer + fav); // print out 42
+    console.log(answer - fav);
+    console.log(answer * fav);
+    console.log(answer / fav);
+
+    let fruit1 = 'apple';   // String data type
+    let fruit2 = "banana";
+    console.log(fruit1 + ' ' + fruit2);
+
+    console.log(`I like ${fruit1}s, but ${fruit2}s, no.`);
+
+    console.log('cat'[1]);
+
+    console.log(answer.toString());
+
+    let determine = 42  == 43;  // The stored value willl be...
+    // ... a Boolean date type ...
+    // ... with false value
+    console.log(determine);
+
+    const name = "Ximena";  // constant value
+    // name = "Something" // It's impossible
+
+    let huh;
+    console.log(huh);   // undefined...
+
+    let droid = 'BB-8';
+    let fromEarth = null;
+    console.log(fromEarth);   
 }
 
 function draw() {
-    background(200, 100, 150);
-    circle(200, 200, 50);
+    background(220);
 }
